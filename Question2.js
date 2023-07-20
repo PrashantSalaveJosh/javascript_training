f8ea7469540bcda15c5ac9a342cdc00b7f7359db
PrashantSalaveJosh <prashant.salave@joshsoftware.com>
2023-07-20 11:01:21 +0530

    completed javascript assignment 3

---
+++ Question2.js
@@ -0,0 +1,31 @@
+/*1. Declare a variable let age = 25;. Write a series using switch case 
+statements that will:
+Print child to the console if age is less than equal to 12.
+Print teen to the console if age is between 13 and 18 (inclusive).
+Print adult to the console if age is above 18
+*/
+
+let age = 25;
+
+const ageChecker = (inputAge) => {
+  switch(true) {
+    case (inputAge < 0):
+      console.log("Age can not be negative");
+      break;
+    case (inputAge <= 12):
+      console.log("Child");
+      break;
+    case (inputAge <= 18):
+      console.log("Teen");
+      break;
+    case (inputAge > 18):
+      console.log("Adult");
+      break;
+    default:
+      console.log("Invalid Input");
+      break;
+  }
+}
+
+ageChecker(age);
+
